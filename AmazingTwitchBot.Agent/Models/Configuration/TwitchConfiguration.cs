4cf2de2902d1db055cab20d2076900e911d618b1
Jim McG <jimmcgowan.uk@gmail.com>
2020-07-01 12:23:57 +0100

    version that build, prepared ahead of stream 1Jul2020, demonstrates working service provider

---
+++ AmazingTwitchBot.Agent/Models/Configuration/TwitchConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace AmazingTwitchBot.Agent.Models.Configuration
+{
+    public class TwitchConfiguration
+    {
+        public string BotToken { get; set; }
+        public string BotUsername { get; set; }
+        public string ClientId { get; set; }
+        public string ChannelName { get; set; }
+    }
+}
+
---
+++ AmazingTwitchBot.Agent/Pinger.cs
@@ -0,0 +1,35 @@
+﻿//using System.Threading;
+
+//namespace AmazingTwitchBot.Agent
+//{
+//    public class Pinger
+//    {
+//        //vars
+//        public TwitchChatBot myClient;
+//        public Thread pingSender;
+
+//        //constructor
+//        public Pinger(TwitchChatBot myClient)
+//        {
+//            this.myClient = myClient;
+//            pingSender = new Thread(new ThreadStart(this.Run));
+//        }
+
+//        // Starts the thread
+//        public void Start()
+//        {
+//            pingSender.IsBackground = true;
+//            pingSender.Start();
+//        }
+
+//        // Send PING to irc server every 5 minutes
+//        public void Run()
+//        {
+//            while (true)
+//            {
+//                myClient.SendPing();
+//                Thread.Sleep(300000); // 5 minutes
+//            }
+//        }
+//    }
+//}
