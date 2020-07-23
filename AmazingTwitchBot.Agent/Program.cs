7e5431b49b8426e82d28e276653ebb65f6a27c9b
Jim McG <jimmcgowan.uk@gmail.com>
2020-07-23 09:38:24 +0100

    Tidy up, after having accepted #PR1 (change to Scrutor assembly scan), prior to merging in #PR2 (shift to a hosted app).

--- AmazingTwitchBot.Agent/Program.cs
+++ AmazingTwitchBot.Agent/Program.cs
@@ -16,8 +16,6 @@ namespace AmazingTwitchBot.Agent
 {
     public class Program
     {
-        // link to inspiration TwitchBot https://github.com/kasuken/SonequaBot (super straightforward/small example)
-
         public static void Main(string[] args)
         {
             IConfiguration configuration = new ConfigurationBuilder()
@@ -31,58 +29,19 @@ namespace AmazingTwitchBot.Agent
             services.Configure<ChatConfiguration>(configuration.GetSection(nameof(ChatConfiguration)));
             services.AddSingleton<TwitchChatBot>();
 
-
-            //var messages = Assembly.GetAssembly(typeof(IChatMessageRule))
-            //    .GetTypes()
-            //    .Where(x => x.Namespace == "AmazingTwitchBot.Agent.Rules.ChatCommands")
-            //    .Where(x => x.IsClass)
-            //    .Select(x => (IChatMessageRule)Activator.CreateInstance(x))
-            //    ;
-
-            //services.AddSingleton(messages);
-
-            // ---- this block could (should) be extracted out into a setup class
-
             services.Scan(scan => scan.FromAssemblyOf<IChatMessageRule>()
                 .AddClasses(classes => classes.AssignableTo<IChatMessageRule>())
-                    // We then specify what type we want to register these classes as.
-                    // In this case, we want to register the types as all of its implemented interfaces.
-                    // So if a type implements 3 interfaces; A, B, C, we'd end up with three separate registrations.
                     .AsImplementedInterfaces()
-                    // And lastly, we specify the lifetime of these registrations.
                     .WithSingletonLifetime()            
             );
-            
-/*
-            services.AddSingleton<List<IChatMessageRule>>();
-            services.AddSingleton<IChatMessageRule, HelloRule>();
-            services.AddSingleton<IChatMessageRule, ProjectRule>();
-            services.AddSingleton<IChatMessageRule, RustySpoonsRule>();
-            services.AddSingleton<IChatMessageRule, SurlyYouCantBeSeriousRule>();
-            services.AddSingleton<IChatMessageRule, SurlyDevClipRule>();
-            services.AddSingleton<IChatMessageRule, DestructoPupRule>();
-            services.AddSingleton<IChatMessageRule, InstagramRule>();
-            services.AddSingleton<IChatMessageRule, TwitterRule>();
-            services.AddSingleton<IChatMessageRule, BlogRule>();
-            // ------------------------------------------------
-*/
+
 
             var serviceProvider = services.BuildServiceProvider();
 
-
-
-
-
-        // https://stackoverflow.com/questions/31863981/how-to-resolve-instance-inside-configureservices-in-asp-net-core
-        //Foo foo = serviceProvider.GetService<Foo>();
-        //ConfigFoo configfoo = serviceProvider.GetService<IOptions<ConfigFoo>>().Value;
-
-        //TwitchConfiguration twitchConfiguration = serviceProvider.GetService<IOptions<TwitchConfiguration>>().Value;
-
-        TwitchChatBot twitchChatBot = serviceProvider.GetService<TwitchChatBot>();
+            TwitchChatBot twitchChatBot = serviceProvider.GetService<TwitchChatBot>();
 
             twitchChatBot.Connect();
-            Console.ReadLine(); // we do this to pause the console app, so it doesn't just run and exit immedietely.
+            Console.ReadLine(); 
 
             twitchChatBot.Disconnect();
 
